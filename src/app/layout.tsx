ff4af4f20b901d61f5187bf76b4d30f21ec0571b
erivaldocazinga22 <cazarthdesogner@gmail.com>
2025-10-29 10:42:05 +0100

    feat: add Analytics Vercel Compomente

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { Footer } from "@/components/layout/footer";
 import { Header } from "@/components/layout/header";
@@ -22,6 +23,7 @@ export default function RootLayout({
 					<main className="flex-1">{children}</main>
 					<Footer />
 				</NuqsAdapter>
+				<Analytics />
 			</body>
 		</html>
 	);
