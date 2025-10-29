e6e7e31954c7d69610b05c956a7fd369d32a862b
erivaldocazinga22 <cazarthdesogner@gmail.com>
2025-10-29 07:27:54 +0100

    feat: first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Diário Do Estilo",
+  description: "O Renascimento da Moda Africana na Cena Global",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt">
+      <body>{children}</body>
+    </html>
+  );
+}
