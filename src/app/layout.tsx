3d328184cdddffe5844013e3ca2d48ce0ddb3104
erivaldocazinga22 <cazarthdesogner@gmail.com>
2025-10-29 10:54:56 +0100

    feat: add  notfound page and suspense in header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { Suspense } from "react";
 import { Footer } from "@/components/layout/footer";
 import { Header } from "@/components/layout/header";
 
@@ -19,7 +20,9 @@ export default function RootLayout({
 		<html lang="pt">
 			<body className="flex flex-col min-h-dvh">
 				<NuqsAdapter>
-					<Header />
+					<Suspense fallback={null}>
+						<Header />
+					</Suspense>
 					<main className="flex-1">{children}</main>
 					<Footer />
 				</NuqsAdapter>
