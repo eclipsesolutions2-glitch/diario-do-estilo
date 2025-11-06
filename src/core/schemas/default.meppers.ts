44ee5dd9bb7468d192622043a61196bee44c6aba
erivaldocazinga22 <cazarthdesogner@gmail.com>
2025-11-06 09:01:39 +0100

    fix: run biome check

--- src/core/schemas/default.meppers.ts
+++ src/core/schemas/default.meppers.ts
@@ -1,29 +1,32 @@
 type SuccessResponse<T> = {
-    success: true
-    data: T
-    message: string
-}
+	success: true;
+	data: T;
+	message: string;
+};
 
 type ErrorResponse = {
-    success: false
-    error: string
-}
+	success: false;
+	error: string;
+};
 
-export type ApiResponse<T = unknown> = SuccessResponse<T> | ErrorResponse
+export type ApiResponse<T = unknown> = SuccessResponse<T> | ErrorResponse;
 
 export const ResponseMapper = {
-    success<T>(data: T, message = "Operação concluída com sucesso"): ApiResponse<T> {
-        return {
-            success: true,
-            data,
-            message,
-        }
-    },
+	success<T>(
+		data: T,
+		message = "Operação concluída com sucesso",
+	): ApiResponse<T> {
+		return {
+			success: true,
+			data,
+			message,
+		};
+	},
 
-    error(error: string): ApiResponse<never> {
-        return {
-            success: false,
-            error,
-        }
-    },
-}
+	error(error: string): ApiResponse<never> {
+		return {
+			success: false,
+			error,
+		};
+	},
+};
