3b2dbfcc2b1cd8ba9f70fb205a219bc93b7c0de9
erivaldocazinga22 <cazarthdesogner@gmail.com>
2025-11-23 21:50:29 +0100

    fix(notification): import in index.js change name of hook action

--- src/core/actions/notification/index.ts
+++ src/core/actions/notification/index.ts
@@ -1,4 +1,4 @@
-import { useNotification } from "./find-many/client";
+import { useFindManyNotifications } from "./find-many/client";
 import { findManyNotificationAction } from "./find-many/server";
 import { findManyUnReadNotificationAction } from "./find-many-unread-notification.action";
 import { markAllReadNotificationAction } from "./mark-all-read-notification.action";
@@ -6,7 +6,7 @@ import { markReadNotificationAction } from "./mark-read-notification.action";
 
 export const notification = {
 	findMany: findManyNotificationAction,
-	useNotification,
+	useNotification: useFindManyNotifications,
 	findManyUnRead: findManyUnReadNotificationAction,
 	markRead: markReadNotificationAction,
 	markAllRead: markAllReadNotificationAction,
