6a7a821ef7d29202ab08594050f2f98465251636
erivaldocazinga22 <cazarthdesogner@gmail.com>
2025-11-28 09:44:57 +0100

    feat(geral): add mapper 'ApiResponseBuilder' in packages

--- packages/ui/src/lib/formats/format-initials.ts
+++ packages/ui/src/lib/formats/format-initials.ts
@@ -1,7 +1,7 @@
 const DEFAULT_STRING_RETURN = "U";
 
 export function formatInitials(name: string) {
-	if (!name) return DEFAULT_STRING_RETURN;
-	const initials = name.split(" ").map((n) => n[0]).join("");
-	return initials.toUpperCase() || DEFAULT_STRING_RETURN;
+    if (!name) return DEFAULT_STRING_RETURN;
+    const initials = name.split(" ").map((n) => n[0]).join("");
+    return initials.toUpperCase() || DEFAULT_STRING_RETURN;
 }
