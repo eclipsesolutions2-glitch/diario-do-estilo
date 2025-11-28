42b3d3884a851a635fa82028da0d1dcb1fcf9729
erivaldocazinga22 <cazarthdesogner@gmail.com>
2025-11-28 14:39:46 +0100

    feat(dashboard_categories): add create, delete, find-many, find-one and update category  actions

---
+++ apps/dashboard/src/core/actions/category/create-category.action.ts
@@ -0,0 +1,48 @@
+"use server";
+import { createCategorySchema, CreateCategorySchemaValues } from "@/core/schemas/category/create-category.schema";
+import { env } from "@/lib/env";
+import { ApiResponse, ApiResponseBuilder } from "@workspace/ui/lib/mappers/api-response-builder.mapper";
+import { cookies } from "next/headers";
+
+export async function createCategoryAction(
+    data: CreateCategorySchemaValues,
+): Promise<ApiResponse<boolean>> {
+    const storage = await cookies();
+    const token = storage.get("dds-auth.session-token");
+    if (!token) {
+        return ApiResponseBuilder.error("Sessão expirada. Faça login novamente.");
+    }
+
+    const parsed = createCategorySchema.safeParse(data);
+
+    if (!parsed.success) {
+        const firstIssue =
+            parsed.error.issues[0]?.message ?? "Dados inválidos.";
+        return ApiResponseBuilder.error(firstIssue);
+    }
+
+    try {
+        const { NEXT_PUBLIC_API_URL } = env;
+        const response = await fetch(`${NEXT_PUBLIC_API_URL}/api/v1/auth/categories`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token.value}`
+            },
+            body: JSON.stringify({
+                name: parsed.data.name,
+                description: parsed.data.description,
+            })
+        });
+        const json = await response.json().catch(() => null);
+        if (!response.ok) {
+            const msg = "Algo correu mal ao tentar criar categoria";
+            return ApiResponseBuilder.error(msg);
+        }
+        return ApiResponseBuilder.success(json);
+    } catch (error) {
+        const errorMessage = "Falha ao criar categoria";
+        console.error(`❌ ERROR: ${errorMessage}`, error);
+        return ApiResponseBuilder.error(errorMessage);
+    }
+}
---
+++ apps/dashboard/src/core/actions/category/delete-category.action.ts
@@ -0,0 +1,39 @@
+"use server";
+import { env } from "@/lib/env";
+import { ApiResponse, ApiResponseBuilder } from "@workspace/ui/lib/mappers/api-response-builder.mapper";
+import { cookies } from "next/headers";
+
+export async function deleteCategoryAction({ slug }: { slug: string }): Promise<ApiResponse<boolean>> {
+    const storage = await cookies();
+    const token = storage.get("dds-auth.session-token");
+    if (!token) {
+        return ApiResponseBuilder.error("Sessão expirada. Faça login novamente.");
+    }
+
+    if (!slug) {
+        return ApiResponseBuilder.error("Dados inválidos");
+    }
+
+    try {
+        const { NEXT_PUBLIC_API_URL } = env;
+        const response = await fetch(`${NEXT_PUBLIC_API_URL}/api/v1/auth/categories/${slug}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token.value}`
+            }
+        });
+
+        const json = await response.json().catch(() => null);
+        if (!response.ok) {
+            const msg = "Algo correu mal ao tentar apagar categoria";
+            return ApiResponseBuilder.error(msg);
+        }
+
+        return ApiResponseBuilder.success(json);
+    } catch (error) {
+        const errorMessage = "Falha ao apagar categoria";
+        console.error(`❌ ERROR: ${errorMessage}`, error);
+        return ApiResponseBuilder.error(errorMessage);
+    }
+}
