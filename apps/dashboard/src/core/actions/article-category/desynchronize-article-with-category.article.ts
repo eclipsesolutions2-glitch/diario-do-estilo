dad95b674f3e88700c4b714b144dad4bb6ce72a1
erivaldocazinga22 <cazarthdesogner@gmail.com>
2025-11-29 20:04:40 +0100

    feat(dashboard_articles): add desynchronize, synchronize category with article actions

---
+++ apps/dashboard/src/core/actions/article-category/desynchronize-article-with-category.article.ts
@@ -0,0 +1,43 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { ApiResponse, ApiResponseBuilder } from "@workspace/ui/lib/mappers/api-response-builder.mapper";
+import { env } from "@/lib/env";
+
+export async function desynchronizeArticleWithCategory(
+    slug: string,
+    categoryId: number,
+): Promise<ApiResponse<boolean>> {
+    const storage = await cookies();
+    const token = storage.get("dds-auth.session-token")?.value;
+
+    if (!token) {
+        return ApiResponseBuilder.error("Sessão expirada. Faça login novamente.");
+    }
+    try {
+        const { NEXT_PUBLIC_API_URL } = env;
+        const response = await fetch(`${NEXT_PUBLIC_API_URL}/api/v1/auth/articles/${slug}/categories`,
+            {
+                method: "DELETE",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify({
+                    category_id: categoryId,
+                }),
+            },
+        );
+
+        if (!response.ok) {
+            const err = await response.text();
+            ApiResponseBuilder.error(
+                err || "Falha ao desassociar a categoria de um artigo",
+            );
+        }
+        return ApiResponseBuilder.success(true);
+    } catch (error) {
+        const errorMessage = "Erro ao desassociar a categoria de um artigo. Verifique os dados e tente novamente.";
+        console.error(`❌ ERROR: ${errorMessage}`, error);
+        return ApiResponseBuilder.error(errorMessage);
+    }
+}
