ed6c7ccd8ab83f9f33bfc4439de3d057a7dfa96f
erivaldocazinga22 <cazarthdesogner@gmail.com>
2025-11-29 20:06:11 +0100

    feat: I'do not

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,45 +1,45 @@
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "@workspace/ui/globals.css";
-import { Providers } from "@/src/components/providers";
+import { Providers } from "@/components/providers";
 import { Metadata } from "next";
 import { Header } from "../components/layout/header";
 import { Footer } from "../components/layout/footer";
 
 const fontSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-sans",
+    subsets: ["latin"],
+    variable: "--font-sans",
 });
 
 const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
+    subsets: ["latin"],
+    variable: "--font-mono",
 });
 
 export const metadata: Metadata = {
-  title: "Diário Do Estilo",
-  description: "O Renascimento da Moda Africana na Cena Global",
-  icons: {
-    icon: "/logo-app.svg",
-  },
+    title: "Diário Do Estilo",
+    description: "O Renascimento da Moda Africana na Cena Global",
+    icons: {
+        icon: "/logo-app.svg",
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="pt" suppressHydrationWarning>
-      <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
-      >
-        <Providers>
-          <Header />
-          {children}
-          <Footer />
-        </Providers>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="pt" suppressHydrationWarning>
+            <body
+                className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
+            >
+                <Providers>
+                    <Header />
+                    {children}
+                    <Footer />
+                </Providers>
+            </body>
+        </html>
+    );
 }
