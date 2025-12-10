ddb33d455cd4dfca4984a37cb6859b57f84eb7ff
erivaldocazinga22 <cazarthdesogner@gmail.com>
2025-12-10 12:47:58 +0100

    feat(web): add suspense in page register

--- apps/web/src/app/(public)/register/page.tsx
+++ apps/web/src/app/(public)/register/page.tsx
@@ -1,25 +1,28 @@
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default function RegisterPage() {
 	return (
-		<div className="space-y-4 pt-20">
-			<div className="text-center">
-				<h1 className="text-3xl font-medium font-serif">
-					Crie sua Conta
-				</h1>
-				<p className="text-sm text-muted-foreground">
-					Junte-se à nossa comunidade de moda
+		<Suspense fallback={null}>
+			<div className="space-y-4 pt-20">
+				<div className="text-center">
+					<h1 className="text-3xl font-medium font-serif">
+						Crie sua Conta
+					</h1>
+					<p className="text-sm text-muted-foreground">
+						Junte-se à nossa comunidade de moda
+					</p>
+				</div>
+				<p className="text-sm text-neutral-500 text-center">
+					Já tem uma conta?{" "}
+					<Link
+						href="/sign-in"
+						className="text-primary font-medium hover:underline"
+					>
+						Faça login
+					</Link>
 				</p>
 			</div>
-			<p className="text-sm text-neutral-500 text-center">
-				Já tem uma conta?{" "}
-				<Link
-					href="/sign-in"
-					className="text-primary font-medium hover:underline"
-				>
-					Faça login
-				</Link>
-			</p>
-		</div>
+		</Suspense>
 	);
 }
